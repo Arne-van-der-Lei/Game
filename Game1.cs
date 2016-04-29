9376bfbaca5d83ad174c9b8bd79ad131445014da
houdhakker2 <arne@vanderlei.be>
2016-04-29 22:48:07 +0200

    irst real commit with current build (map and avatar working

---
+++ Game1.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.IO;
+
+namespace Game3
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        BasicEffect effect;
+        VertexPositionColor[] ava;
+        Map map;
+        int distance;
+        Vector3 avatarPos;
+        bool down;
+        bool right;
+        bool left;
+        bool up;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            //fullscreen
+            graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+            graphics.IsFullScreen = true;
+            graphics.ApplyChanges();
+
+            //neirest neighbour
+            GraphicsDevice.SamplerStates[0] = SamplerState.PointWrap;
+
+            //avatar model
+            ava = new VertexPositionColor[6];
+            ava[0].Position = new Vector3(-.5f, 1f, -.5f);
+            ava[1].Position = new Vector3( .5f, 1f, -.5f);
+            ava[2].Position = new Vector3(-.5f, 0f,  .5f);
+
+            ava[3].Position = new Vector3( .5f, 1f, -.5f);
+            ava[4].Position = new Vector3( .5f, 0f,  .5f);
+            ava[5].Position = new Vector3(-.5f, 0f,  .5f);
+
+            ava[0].Color = new Color(127, 127, 127);
+            ava[1].Color = new Color(127, 127, 127);
+            ava[2].Color = new Color(127, 127, 127);
+
+            ava[3].Color = new Color(127, 127, 127);
+            ava[4].Color = new Color(127, 127, 127);
+            ava[5].Color = new Color(127, 127, 127);
+
+            //init mapdata -> moet nog geport worden naar file
+            map = new Map();
+            map.Tiles = new int[,]
+            {
+                {2 , 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+                {1 , 1, 1, 1, 1, 2, 2, 1, 0, 0, 0, 1},
+                {1 , 0, 0, 0, 1, 2, 2, 1, 0, 0, 0, 1},
+                {1 , 0, 0, 0, 1, 2, 2, 1, 0, 0, 1, 1},
+                {1 , 0, 0, 0, 1, 2, 2, 1, 0, 0, 1, 1},
+                {1 , 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1},
+                {1 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1 , 0,-1,-1, 0, 1, 1, 1, 1, 1, 1, 1},
+                {1 , 0,-1,-1, 0, 1, 2, 2, 2, 2, 2, 2}
+            };
+            map.IdTiles = new int[,]
+            {
+                {2 ,2 ,2 ,2 ,3 ,5 ,4 ,0 ,2 ,2 ,3 },
+                {0 ,2 ,2 ,3 ,7 ,5 ,4 ,4 ,5 ,5 ,7 },
+                {4 ,9 ,6 ,7 ,7 ,5 ,4 ,4 ,6 ,0 ,15},
+                {4 ,6 ,5 ,7 ,7 ,5 ,4 ,4 ,5 ,7 ,5 },
+                {4 ,5 ,5 ,7 ,0 ,2 ,0 ,4 ,5 ,0 ,3 },
+                {4 ,5 ,9 ,0 ,2 ,2 ,2 ,0 ,5 ,5 ,7 },
+                {4 ,9 ,5 ,5 ,6 ,5 ,10,5 ,6 ,5 ,7 }, 
+                {4 ,5 ,6 ,5 ,5 ,9 ,5 ,6 ,5 ,5 ,7 },
+                {12,2 ,13,3 ,0 ,13,13,13,13,13,15},
+                {4 ,4 ,5 ,7 ,7 ,13,13,13,13,13,15}
+            };
+
+
+            //avatar + cam ofset
+            avatarPos = new Vector3(1.5f, 0, 2.5f);
+            distance = 3;
+
+            //shader
+            effect = new BasicEffect(GraphicsDevice);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            //spritebatch for 2d stuff
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            //map generation + textue setting
+            map.Texture = Content.Load<Texture2D>("Tiles_0");
+            map.generate();
+
+            //effect = Content.Load<Effect>("shader");
+            //map = Content.Load<Map>("map");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            graphics.ApplyChanges();
+
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            //movement
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                if(map.Hitbox[(int)(avatarPos.Z+.45f), (int)(avatarPos.X + .5f)] == 0 && map.Hitbox[(int)(avatarPos.Z-.45f), (int)(avatarPos.X + .5f)] == 0)
+                {
+                    avatarPos.X += 2f*(float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                if (map.Hitbox[(int)(avatarPos.Z - .5f), (int)(avatarPos.X +.45f)] == 0 && map.Hitbox[(int)(avatarPos.Z - .5f), (int)(avatarPos.X - .45f)] == 0)
+                {
+                    avatarPos.Z -= 2f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                if (map.Hitbox[(int)(avatarPos.Z + .45f), (int)(avatarPos.X - .5f)] == 0 && map.Hitbox[(int)(avatarPos.Z - .45f), (int)(avatarPos.X - .5f)] == 0)
+                {
+                    avatarPos.X -= 2f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                if (map.Hitbox[(int)(avatarPos.Z + .5f), (int)(avatarPos.X - .45f)] == 0 && map.Hitbox[(int)(avatarPos.Z + .5f), (int)(avatarPos.X + .45f)] == 0)
+                {
+                    avatarPos.Z += 2f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            var cameraPosition = new Vector3(avatarPos.X, avatarPos.Y + distance + 1, avatarPos.Z + distance);
+            var cameraLookAtVector = avatarPos;
+            var cameraUpVector = Vector3.Up;
+
+            effect.View = Matrix.CreateLookAt(cameraPosition, cameraLookAtVector, cameraUpVector);
+
+            float aspectRatio = graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight;
+            float fieldOfView = MathHelper.ToRadians(70f);
+            float nearClipPlane = 1;
+            float farClipPlane = 1000;
+
+            effect.Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
+            DrawGround();
+            DrawAvatar();
+            base.Draw(gameTime);
+        }
+
+        void DrawGround()
+        {
+            effect.TextureEnabled = true;
+            effect.Texture = map.Texture;
+
+            //draw map
+            effect.World = Matrix.CreateTranslation(0, 0, 0);
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList,map.mesh,0,map.mesh.GetLength(0)/3);
+            }
+        }
+
+        void DrawAvatar()
+        {
+            effect.World = Matrix.CreateTranslation(avatarPos);
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, ava, 0, ava.GetLength(0) / 3);
+            }
+        }
+    }
+}
---
+++ Map.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game3
+{
+    public class Map
+    {
+        public int[,] Tiles { get; set; }
+        public int[,] Hitbox { get; private set; }
+        public int[,] IdTiles { get; set; }
+        public Texture2D Texture { get; set; }
+
+        public VertexPositionTexture[] mesh;
+        public void generate()
+        {
+            Hitbox = new int[Tiles.GetLength(0)-1,Tiles.GetLength(1)-1];
+            mesh = new VertexPositionTexture[Tiles.GetLength(0) * Tiles.GetLength(1) * 6];
+            int width = Tiles.GetLength(0);
+            float withTile =0.25f, HeightTile = 0.1428f;
+            for (int i = 0; i < Tiles.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < Tiles.GetUpperBound(1); j++)
+                {
+                    if (Tiles[i, j] == Tiles[i + 1, j + 1])
+                    {
+                        mesh[(j * width + i) * 6].Position = new Vector3(j,  Tiles[i, j], i);
+                        mesh[(j * width + i) * 6 + 2].Position = new Vector3(j,  Tiles[i + 1, j ], i + 1);
+                        mesh[(j * width + i) * 6 + 1].Position = new Vector3(j + 1, Tiles[i , j + 1], i);
+                        mesh[(j * width + i) * 6 + 3].Position = new Vector3(j,  Tiles[i + 1, j ], i + 1);
+                        mesh[(j * width + i) * 6 + 5].Position = new Vector3(j + 1,  Tiles[i + 1, j + 1], i + 1);
+                        mesh[(j * width + i) * 6 + 4].Position = new Vector3(j + 1,  Tiles[i , j + 1], i);
+
+                        mesh[(j * width + i) * 6].TextureCoordinate =     new Vector2((IdTiles[i, j] % 4) * withTile            , (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 1].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile + withTile , (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 2].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile            , (IdTiles[i, j] / 4) * HeightTile  + HeightTile);
+                        mesh[(j * width + i) * 6 + 3].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile            , (IdTiles[i, j] / 4) * HeightTile  + HeightTile);
+                        mesh[(j * width + i) * 6 + 4].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile + withTile , (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 5].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile + withTile , (IdTiles[i, j] / 4) * HeightTile  + HeightTile);
+                    }
+                    else
+                    {
+                        mesh[(j * width + i) * 6].Position = new Vector3(j + 1,  Tiles[i, j + 1], i);
+                        mesh[(j * width + i) * 6 + 2].Position = new Vector3(j,  Tiles[i, j], i);
+                        mesh[(j * width + i) * 6 + 1].Position = new Vector3(j + 1,  Tiles[i + 1, j + 1], i + 1);
+                        mesh[(j * width + i) * 6 + 3].Position = new Vector3(j,  Tiles[i, j], i);
+                        mesh[(j * width + i) * 6 + 5].Position = new Vector3(j,  Tiles[i + 1, j], i + 1);
+                        mesh[(j * width + i) * 6 + 4].Position = new Vector3(j + 1,  Tiles[i + 1, j + 1], i + 1);
+                        
+                        mesh[(j * width + i) * 6].TextureCoordinate =     new Vector2((IdTiles[i, j] % 4) * withTile + withTile, (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 1].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile + withTile, (IdTiles[i, j] / 4) * HeightTile + HeightTile);
+                        mesh[(j * width + i) * 6 + 2].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile           , (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 3].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile           , (IdTiles[i, j] / 4) * HeightTile);
+                        mesh[(j * width + i) * 6 + 4].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile + withTile, (IdTiles[i, j] / 4) * HeightTile + HeightTile);
+                        mesh[(j * width + i) * 6 + 5].TextureCoordinate = new Vector2((IdTiles[i, j] % 4) * withTile           , (IdTiles[i, j] / 4) * HeightTile + HeightTile);
+                    }
+
+                    Hitbox[i, j] = Tiles[i, j] == Tiles[i + 1, j]  && Tiles[i, j + 1] == Tiles[i + 1, j + 1] && Tiles[i, j] == Tiles[i + 1, j + 1] ? 0 : 1 ;
+                }
+            }
+        }
+
+        public void createHeightMap(string map,int width , int height)
+        {
+            string[] strArr = map.Substring(1,map.Length-2).Split(",",StringSplitOptions.None);
+            for (int i = 0; i < width; i++)
+            {
+                for( int j = 0; j < height; j++)
+                {
+                    switch ()
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
